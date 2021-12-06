3b027a087010f85849fa8688258c0ad949112386
MumriyAlex <mumriyalex@gmail.com>
2021-12-06 11:57:20 +0200

    06-12-2021 11:57

--- MyFirstDjangoProject/newyear/views.py
+++ MyFirstDjangoProject/newyear/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("NewYear!!!!!")
+#___________________________________
 def index(request):
-    return HttpResponse("NewYear!!!!!")
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
