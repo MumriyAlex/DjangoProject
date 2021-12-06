5fcb8dbff9feb437ebb21a3d2b307b0bda766899
MumriyAlex <mumriyalex@gmail.com>
2021-12-06 11:37:57 +0200

    06-12-2021 11:37

--- MyFirstDjangoProject/MyFirstDjangoProject/urls.py
+++ MyFirstDjangoProject/MyFirstDjangoProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("Hello.urls"))
+    path('hello/', include("Hello.urls")),
+    path('newyear/', include("newyear.urls"))
 ]
